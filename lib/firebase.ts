93609ddbfa3474eb6e9555359f4ad26ac5445ba4
a96363877 <a96363877@gmail.com>
2025-06-29 02:55:33 +0300

    d

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -8,15 +8,15 @@ import {
   } from "firebase/firestore";
   import { initializeApp } from "firebase/app";
   import { getDatabase } from "firebase/database";
-  
-  const firebaseConfig = {
-    apiKey: "AIzaSyD8b2GhSgTi9-p61mTfP0OoRw7DofJIXoo",
-  authDomain: "sxzxc-c1acb.firebaseapp.com",
-  projectId: "sxzxc-c1acb",
-  storageBucket: "sxzxc-c1acb.firebasestorage.app",
-  messagingSenderId: "882959697671",
-  appId: "1:882959697671:web:337b14ae8a05aeb876e7a2",
-  measurementId: "G-43WVGYQQ38"
+ 
+   const firebaseConfig = {
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
   };
   
   const app = initializeApp(firebaseConfig);
